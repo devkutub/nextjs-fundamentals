2af11dcac3fc1e53b28a0a90740b72f9402f4b32
Kutub <kutubuddin2528@gmail.com>
2023-10-18 19:44:45 +0530

    added tailwind css

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import AddToCart from './AddToCart'
-import styles from "./ProductCard.module.css";
+// import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
     return (
-        <div className={styles.card}>
+        // <div className={styles.card}>
+        <div className="p-4 my-5 bg-sky-300 text-xl hover:bg-sky-500">
             <AddToCart />
         </div>
     )
