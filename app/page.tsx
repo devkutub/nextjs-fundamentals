205590cb10495146d1886eb4e7f898e2f9b88db5
Kutub <kutubuddin2528@gmail.com>
2023-10-18 17:28:41 +0530

    added client & server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import ProductCard from './components/productcard'
 
 export default function Home() {
   return (
     <main>
       <h1>Hey NextJS</h1>
       <Link href="/users">Go to users page</Link>
+      <ProductCard />
     </main>
   )
 }
