3a1abeaccb26a72009ddd0eb0bb6413513840c3a
Kutub <kutubuddin2528@gmail.com>
2023-10-18 17:46:35 +0530

    added data fetching and cache

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const UsersPage = () => {
+interface User {
+    id: number;
+    name: string;
+}
+
+const UsersPage = async () => {
+    const res = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+        {
+            // cache: "no-store", // next will not store the data in cache
+            next: { revalidate: 10 } // if this option enabled then cache option should be removed and next will run a job every 10 seconds to get the fresh data in background
+        }
+    );
+    const users: User[] = await res.json();
+
     return (
-        <div>UsersPage</div>
+        <>
+            <h1>Users</h1>
+            <ul>{users.map((d) => <li key={d.id}>{d.name}</li>)}</ul>
+        </>
     )
 }
 
