2e6e7ce33bf127c0297bba00ef1f5a61a50e04cd
Kutub <kutubuddin2528@gmail.com>
2023-10-24 12:24:09 +0530

    added put call to update user object

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -11,3 +11,21 @@ export function GET(
 
     return NextResponse.json({ id: 1, name: " Kutub" });
 }
+
+
+export async function PUT(
+    request: NextRequest,
+    { params }: { params: { id: number } }
+) {
+    // validate the request
+    // if not valid return 400
+    const body = await request.json();
+    if (!body.name) return NextResponse.json({ error: "Name os required" }, { status: 400 });
+    // fetch the user from db
+    // if doesn't exist return 404
+    if (params.id > 10) return NextResponse.json({ error: 'user not found' }, { status: 404 });
+    // update the user object
+    // return the updated user data
+
+    return NextResponse.json({ id: 1, ...body });
+}
