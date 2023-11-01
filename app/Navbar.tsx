4359b05a3e208bae5662606038a6bf6e8910ea23
Kutub <kutubuddin2528@gmail.com>
2023-11-01 20:33:17 +0530

    configuring google provider

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,17 +2,18 @@ import Link from "next/link";
 import { FunctionComponent } from "react";
 
 interface NavbarProps {
-    
+
 }
- 
+
 const Navbar: FunctionComponent<NavbarProps> = () => {
     return (
-        <nav className="bg-slate-200 p-4">
+        <nav className="bg-slate-200 p-4 space-x-4">
             <Link href="/" className="mr-8">Next.Js</Link>
-            <Link href="/users" className="mr-4">users</Link>
+            <Link href="/users">users</Link>
             <Link href="/admin">Admins</Link>
+            <Link href="/api/auth/signin">LogIn</Link>
         </nav>
     );
 }
- 
+
 export default Navbar;
