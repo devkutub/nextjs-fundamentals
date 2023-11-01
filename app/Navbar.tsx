6d6cf04d2a0d8786503dc120d6c0d3203786198e
Kutub <kutubuddin2528@gmail.com>
2023-11-01 21:03:33 +0530

    access session on client

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,17 +1,21 @@
+'use client';
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { FunctionComponent } from "react";
 
-interface NavbarProps {
-
-}
+interface NavbarProps { }
 
 const Navbar: FunctionComponent<NavbarProps> = () => {
+    const { status, data: session } = useSession();
+
     return (
-        <nav className="bg-slate-200 p-4 space-x-4">
+        <nav className="bg-slate-200 p-4 space-x-4 flex">
             <Link href="/" className="mr-8">Next.Js</Link>
             <Link href="/users">users</Link>
             <Link href="/admin">Admins</Link>
-            <Link href="/api/auth/signin">LogIn</Link>
+            <div className="flex-grow"></div>
+            {status === "authenticated" && <div>{session.user?.name}</div>}
+            {status === "unauthenticated" && <Link href="/api/auth/signin">LogIn</Link>}
         </nav>
     );
 }
---
+++ app/auth/Provider.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { SessionProvider } from "next-auth/react";
+import { FunctionComponent, ReactNode } from "react";
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+const AuthProvider: FunctionComponent<AuthProviderProps> = ({ children }) => {
+    return (
+        <SessionProvider>{children}</SessionProvider>
+    );
+}
+
+export default AuthProvider;
