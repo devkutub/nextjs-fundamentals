55081a0d39da9fc4e00415a26f4b4ba121d2aef2
Kutub <kutubuddin2528@gmail.com>
2023-11-01 19:33:05 +0530

    showing uploaded cloudinary image

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,28 @@
 'use client';
-import { FunctionComponent } from "react";
-import { CldUploadWidget } from 'next-cloudinary';
+import { FunctionComponent, useState } from "react";
+import { CldUploadWidget, CldImage } from 'next-cloudinary';
 
-interface UploadWidgetPageProps {
+interface UploadWidgetPageProps { }
 
+interface CloudinaryResult {
+    public_id: string;
 }
 
 const UploadWidgetPage: FunctionComponent<UploadWidgetPageProps> = () => {
+    const [publicId, setPublicId] = useState('' as string);
     return (
-        <CldUploadWidget uploadPreset="wxmopopt">
-            {({ open }) => <button className="btn btn-primary" onClick={() => open()}>Upload</button>}
-        </CldUploadWidget>
+        <>
+            {publicId && <CldImage src={publicId} width={270} height={180} alt="fragrance" />}
+            <CldUploadWidget
+                uploadPreset="wxmopopt"
+                onUpload={(result) => {
+                    const info = result.info as CloudinaryResult;
+                    if (info.public_id) setPublicId(info.public_id);
+                }}
+            >
+                {({ open }) => <button className="btn btn-primary" onClick={() => open()}>Upload</button>}
+            </CldUploadWidget>
+        </>
     );
 }
 
