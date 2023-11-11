a7e6513231dbb49334ef9be7bec11ddb01867ae3
Kutub <kutubuddin2528@gmail.com>
2023-11-11 17:39:33 +0530

    added lazy loading for components & external js library like lodash

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,40 @@
-import { FunctionComponent } from "react";
+'use client';
+import { FunctionComponent, useState } from "react";
+// import HeavyComponent from "../components/HeavyComponent";
+import dynamic from "next/dynamic";
+const HeavyComponent = dynamic(
+    () => import("../components/HeavyComponent"),
+    {
+        ssr: false,
+        loading: () => <p>...loading</p>
+    }
+); // lazy loading, loaded when required, need to use for loaded component or else there will be no use of it
+// import _ from "lodash"; // static import
 
 interface AdminHomePageProps {
-    
+
 }
- 
+
 const AdminHomePage: FunctionComponent<AdminHomePageProps> = () => {
+    const [visible, setVisible] = useState(false);
     return (
-        <h1>Admin Home Page</h1>
+        <>
+            <h1>Admin Home Page</h1>
+            <button className="btn btn-primary" onClick={() => setVisible(true)}>Show</button>
+            {visible && <HeavyComponent />}
+            <button className="btn btn-secondary" onClick={async () => {
+                const _ = (await import("lodash")).default; // lazy loading external javascript library
+                const users = [
+                    { name: "c" },
+                    { name: "b" },
+                    { name: "a" },
+                ];
+
+                const sortedUser = _.orderBy(users, ["name"]);
+                console.log(sortedUser);
+            }}>Lodash Button</button>
+        </>
     );
 }
- 
+
 export default AdminHomePage;
---
+++ app/components/HeavyComponent.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+interface HeavyComponentProps {
+
+}
+
+const HeavyComponent: FunctionComponent<HeavyComponentProps> = () => {
+    return (
+        <h3>My Heavy Component</h3>
+    );
+}
+
+export default HeavyComponent;
